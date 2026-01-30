53b36725d315c7f41e551aceb771344c0b9bed67
MarcAureleAgbo <dastafr@gmail.com>
2026-01-30 23:47:45 +0100

    Fix: serve XML at /sitemap.xml (return proper Content-Type)

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,8 +1,9 @@
 export async function GET() {
-  return new Response(null, {
-    status: 301,
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <sitemap>\n    <loc>https://calcery.pages.dev/sitemap-0.xml</loc>\n  </sitemap>\n</sitemapindex>`;
+  return new Response(xml, {
+    status: 200,
     headers: {
-      Location: '/sitemap-index.xml',
+      'Content-Type': 'application/xml; charset=utf-8',
     },
   });
 }
